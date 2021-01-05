3306252bd83ff8a6df577042a1113a3507fe927b
radpac <195882@edu.p.lodz.pl>
2021-01-05 14:18:55 +0100

    Initial Commit

---
+++ BitTick/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BitTick
+//
+//  Created by Radek Pachla on 05/01/2021.
+//  Copyright © 2021 Radek Pachla. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BitTick/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BitTick
+//
+//  Created by Radek Pachla on 05/01/2021.
+//  Copyright © 2021 Radek Pachla. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
